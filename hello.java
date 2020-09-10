a747bb693f041d3886334d81f185ed521e2380f3
yibambay <acmas44@gmail.com>
2020-09-10 10:48:28 +0000

    Welcoming hello.py too.

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
-class Hello
-{
-public Static void main(String args[])
-	{
-		System.out.println("\nHello World!\n");
-	}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
